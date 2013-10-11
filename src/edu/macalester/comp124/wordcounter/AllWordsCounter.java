ff7ff498938ab1e3a9aed3ac17671530cd752ab2
Cole <cericks4@macalester.edu>
2013-10-11 10:53:12 -0500

    Made Changes

--- src/edu/macalester/comp124/wordcounter/AllWordsCounter.java
+++ src/edu/macalester/comp124/wordcounter/AllWordsCounter.java
@@ -12,13 +12,17 @@ public class AllWordsCounter {
 
     public static final int MAX_WORDS = 10000;
 
-	// TODO: initialize instance variable to hold MAX_WORDS objects
-    SingleWordCounter counters[];
+    SingleWordCounter counters[] = new SingleWordCounter[MAX_WORDS];
 
     public int getNumWords() {
-        // TODO: count the number of distinct words,
         // ie. the number of non-null counter objects.
-        return -1;
+        int counter = 0;
+        for (int i = 0; i < counters.length; i++){
+            if(counters[i] != null){
+                counter++;
+            }
+        }
+        return counter;
     }
 	
 	/**
@@ -29,12 +33,20 @@ public class AllWordsCounter {
 	 */
 	public void count(String word) {
         int n = getNumWords();
+        boolean found = false;
         for (int i = 0; i < n; i++) {
             // If you find the word increment the count and return
+            if (counters[i].wordMatches(word)) {
+                counters[i].incrementCount();
+                found = true;
+            }
+            // You didn't find the word. Add a new word counter to the array.
+            // Don't forget to increment the word's count to get it to 1!
         }
-
-        // You didn't find the word. Add a new word counter to the array.
-        // Don't forget to increment the word's count to get it to 1!
+        if (found == false){
+            counters[n] = new SingleWordCounter (word);
+            counters[n].incrementCount();
+        }
 	}
 	
 	/**
@@ -44,10 +56,19 @@ public class AllWordsCounter {
 	 * @return
 	 */
 	public int getCount(String word) {
-        // TODO: pattern this after the count() function.
         // Make sure to return 0 for words you haven't seen before.
-        return -1;
-	}
+        int n = getNumWords();
+        for (int i = 0; i < n; i++) {
+            // If you find the word increment the count and return
+            if (counters[i].wordMatches(word)) {
+                return counters[i].getCount();
+            }
+            // You didn't find the word. Add a new word counter to the array.
+            // Don't forget to increment the word's count to get it to 1!
+        }
+        return 0;
+    }
+
 	
 	/**
 	 * @return The an array of all words that have been counted
